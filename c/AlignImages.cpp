5a357c1d991b65c1a978e8830c36b9830e62c87d
Mark Winter <mwinter@drexel.edu>
2013-05-03 17:31:04 -0400

    Fixes for several off-by-one errors in our max overlap calculations.

--- c/AlignImages.cpp
+++ c/AlignImages.cpp
@@ -17,17 +17,17 @@ void align()
 			ov.deltaXss = gImageTiffs[overlapImageInd]->getXPosition()/gImageTiffs[staticImageInd]->getXPixelPhysicalSize() - gImageTiffs[staticImageInd]->getXPosition()/gImageTiffs[staticImageInd]->getXPixelPhysicalSize();
 			ov.deltaXse = ov.deltaXss + gImageTiffs[overlapImageInd]->getXSize();
 			ov.deltaXmax = min(MARGIN, gImageTiffs[staticImageInd]->getXSize() - MIN_OVERLAP - ov.deltaXss);
-			ov.deltaXmin = max(-MARGIN, MIN_OVERLAP - ov.deltaXse);
+			ov.deltaXmin = max(-MARGIN, (MIN_OVERLAP-1) - ov.deltaXse);
 
 			ov.deltaYss = gImageTiffs[overlapImageInd]->getYPosition()/gImageTiffs[staticImageInd]->getYPixelPhysicalSize() - gImageTiffs[staticImageInd]->getYPosition()/gImageTiffs[staticImageInd]->getYPixelPhysicalSize();
 			ov.deltaYse = ov.deltaYss + gImageTiffs[overlapImageInd]->getYSize();
 			ov.deltaYmax = min(MARGIN, gImageTiffs[staticImageInd]->getYSize() - MIN_OVERLAP - ov.deltaYss);
-			ov.deltaYmin = max(-MARGIN, MIN_OVERLAP - ov.deltaYse);
+			ov.deltaYmin = max(-MARGIN, (MIN_OVERLAP-1) - ov.deltaYse);
 
 			ov.deltaZss = gImageTiffs[overlapImageInd]->getZPosition()/gImageTiffs[staticImageInd]->getZPixelPhysicalSize() - gImageTiffs[staticImageInd]->getZPosition()/gImageTiffs[staticImageInd]->getZPixelPhysicalSize();
 			ov.deltaZse = ov.deltaZss + gImageTiffs[overlapImageInd]->getZSize();
 			ov.deltaZmax = min(MARGIN, gImageTiffs[staticImageInd]->getZSize() - MIN_OVERLAP_Z - ov.deltaZss);
-			ov.deltaZmin = max(-MARGIN, MIN_OVERLAP_Z - ov.deltaZse);
+			ov.deltaZmin = max(-MARGIN, (MIN_OVERLAP_Z-1) - ov.deltaZse);
 
 			if (ov.deltaXmax-ov.deltaXmin<=0 && ov.deltaYmax-ov.deltaYmin<=0) // && all dims
 				continue;
