3019f33240d98edb7d80ed474e484be496d39897
Eric Wait <eric@waitphoto.com>
2013-05-04 09:34:13 -0400

    Handle Error no longer exits

--- c/CudaUtilities.h
+++ c/CudaUtilities.h
@@ -10,7 +10,7 @@ static void HandleError( cudaError_t err, const char *file,	int line )
 	{
 		//TODO Handle cuda error somehow
 		 		printf( "%s in %s at line %d\n", cudaGetErrorString( err ),	file, line );
-		 		exit(EXIT_FAILURE);
+		 		//exit(EXIT_FAILURE);
 	}
 }
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
