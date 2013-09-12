45a7395566063dcfb268d60fec3b6aa597fbd268
Eric Wait <eric@waitphoto.com>
2013-09-12 08:58:16 -0400

    Fixed MaxROI calculation

--- c/RidgidRegistration.cu
+++ c/RidgidRegistration.cu
@@ -60,19 +60,21 @@ double calcCorrelation(CudaImageBuffer<float>& staticIm, CudaImageBuffer<float>&
 void calcMaxROIs(const Overlap& overlap, Vec<int> imageExtents, comparedImages<unsigned int>& imStarts,
 	comparedImages<unsigned int>& imSizes, Vec<int>& maxOverlapSize)
 {
-	imStarts.staticIm.x = (int)(pow(2.0,(int)sizeof(int)));
-	imStarts.staticIm.y = (int)(pow(2.0,(int)sizeof(int)));
-	imStarts.staticIm.z = (int)(pow(2.0,(int)sizeof(int)));
-	imStarts.overlapIm.x = (int)(pow(2.0,(int)sizeof(int)));
-	imStarts.overlapIm.y = (int)(pow(2.0,(int)sizeof(int)));
-	imStarts.overlapIm.z = (int)(pow(2.0,(int)sizeof(int)));
+	comparedImages<int> localStarts;
+	comparedImages<int> localSizes;
+	localStarts.staticIm.x = std::numeric_limits<int>::max();
+	localStarts.staticIm.y = std::numeric_limits<int>::max();
+	localStarts.staticIm.z = std::numeric_limits<int>::max();
+	localStarts.overlapIm.x = std::numeric_limits<int>::max();
+	localStarts.overlapIm.y = std::numeric_limits<int>::max();
+	localStarts.overlapIm.z = std::numeric_limits<int>::max();
 
-	imSizes.staticIm.x = 0;
-	imSizes.staticIm.y = 0;
-	imSizes.staticIm.z = 0;
-	imSizes.overlapIm.x = 0;
-	imSizes.overlapIm.y = 0;
-	imSizes.overlapIm.z = 0;
+	localSizes.staticIm.x = 0;
+	localSizes.staticIm.y = 0;
+	localSizes.staticIm.z = 0;
+	localSizes.overlapIm.x = 0;
+	localSizes.overlapIm.y = 0;
+	localSizes.overlapIm.z = 0;
 
 	for (int deltaX=overlap.deltaXmin; deltaX<overlap.deltaXmax; ++deltaX)
 	{
@@ -80,36 +82,36 @@ void calcMaxROIs(const Overlap& overlap, Vec<int> imageExtents, comparedImages<u
 		{
 			for (int deltaZ=overlap.deltaZmin; deltaZ<overlap.deltaZmax; ++deltaZ)
 			{
-				comparedImages<unsigned int> mins;
+				comparedImages<int> mins;
 				Vec<int> szs;
 
-				mins.staticIm.x = (unsigned int)std::max<int>(0,overlap.deltaXss+deltaX);
-				mins.overlapIm.x = (unsigned int)std::max<int>(0,-(overlap.deltaXss+deltaX));
+				mins.staticIm.x = std::max<int>(0,overlap.deltaXss+deltaX);
+				mins.overlapIm.x = std::max<int>(0,-(overlap.deltaXss+deltaX));
 				szs.x = std::min<int>(imageExtents.x,overlap.deltaXse+deltaX+1) - mins.staticIm.x;
 
-				mins.staticIm.y = (unsigned int)std::max<int>(0,overlap.deltaYss+deltaY);
-				mins.overlapIm.y = (unsigned int)std::max<int>(0,-(overlap.deltaYss+deltaY));
-				szs.y = std::min<int>(imageExtents.x,overlap.deltaYse+deltaY+1) - mins.staticIm.y;
+				mins.staticIm.y = std::max<int>(0,overlap.deltaYss+deltaY);
+				mins.overlapIm.y = std::max<int>(0,-(overlap.deltaYss+deltaY));
+				szs.y = std::min<int>(imageExtents.y,overlap.deltaYse+deltaY+1) - mins.staticIm.y;
 
-				mins.staticIm.z = (unsigned int)std::max<int>(0,overlap.deltaZss+deltaZ);
-				mins.overlapIm.z = (unsigned int)std::max<int>(0,-(overlap.deltaZss+deltaZ));
+				mins.staticIm.z = std::max<int>(0,overlap.deltaZss+deltaZ);
+				mins.overlapIm.z = std::max<int>(0,-(overlap.deltaZss+deltaZ));
 				szs.z = std::min<int>(imageExtents.z,overlap.deltaZse+deltaZ+1) - mins.staticIm.z;
 
-				imStarts.staticIm.x = (unsigned int)std::min<int>(imStarts.staticIm.x,mins.staticIm.x);
-				imStarts.staticIm.y = (unsigned int)std::min<int>(imStarts.staticIm.y,mins.staticIm.y);
-				imStarts.staticIm.z = (unsigned int)std::min<int>(imStarts.staticIm.z,mins.staticIm.z);
+				localStarts.staticIm.x = std::min<int>(localStarts.staticIm.x,mins.staticIm.x);
+				localStarts.staticIm.y = std::min<int>(localStarts.staticIm.y,mins.staticIm.y);
+				localStarts.staticIm.z = std::min<int>(localStarts.staticIm.z,mins.staticIm.z);
 
-				imSizes.staticIm.x = (unsigned int)std::max<int>(imSizes.staticIm.x,mins.staticIm.x+szs.x);
-				imSizes.staticIm.y = (unsigned int)std::max<int>(imSizes.staticIm.y,mins.staticIm.y+szs.y);
-				imSizes.staticIm.z = (unsigned int)std::max<int>(imSizes.staticIm.z,mins.staticIm.z+szs.z);
+				localSizes.staticIm.x = std::max<int>(localSizes.staticIm.x,mins.staticIm.x+szs.x);
+				localSizes.staticIm.y = std::max<int>(localSizes.staticIm.y,mins.staticIm.y+szs.y);
+				localSizes.staticIm.z = std::max<int>(localSizes.staticIm.z,mins.staticIm.z+szs.z);
 
-				imStarts.overlapIm.x = (unsigned int)std::min<int>(imStarts.overlapIm.x,mins.overlapIm.x);
-				imStarts.overlapIm.y = (unsigned int)std::min<int>(imStarts.overlapIm.y,mins.overlapIm.y);
-				imStarts.overlapIm.z = (unsigned int)std::min<int>(imStarts.overlapIm.z,mins.overlapIm.z);
+				localStarts.overlapIm.x = std::min<int>(localStarts.overlapIm.x,mins.overlapIm.x);
+				localStarts.overlapIm.y = std::min<int>(localStarts.overlapIm.y,mins.overlapIm.y);
+				localStarts.overlapIm.z = std::min<int>(localStarts.overlapIm.z,mins.overlapIm.z);
 
-				imSizes.overlapIm.x = (unsigned int)std::max<int>(imSizes.overlapIm.x,mins.overlapIm.x+szs.x);
-				imSizes.overlapIm.y = (unsigned int)std::max<int>(imSizes.overlapIm.y,mins.overlapIm.y+szs.y);
-				imSizes.overlapIm.z = (unsigned int)std::max<int>(imSizes.overlapIm.z,mins.overlapIm.z+szs.z);
+				localSizes.overlapIm.x = std::max<int>(localSizes.overlapIm.x,mins.overlapIm.x+szs.x);
+				localSizes.overlapIm.y = std::max<int>(localSizes.overlapIm.y,mins.overlapIm.y+szs.y);
+				localSizes.overlapIm.z = std::max<int>(localSizes.overlapIm.z,mins.overlapIm.z+szs.z);
 
 				maxOverlapSize.x = std::max<int>(maxOverlapSize.x,szs.x);
 				maxOverlapSize.y = std::max<int>(maxOverlapSize.y,szs.y);
@@ -118,12 +120,25 @@ void calcMaxROIs(const Overlap& overlap, Vec<int> imageExtents, comparedImages<u
 		}
 	}
 
-	imSizes.staticIm.x -= imStarts.staticIm.x;
-	imSizes.staticIm.y -= imStarts.staticIm.y;
-	imSizes.staticIm.z -= imStarts.staticIm.z;
-	imSizes.overlapIm.x -= imStarts.overlapIm.x;
-	imSizes.overlapIm.y -= imStarts.overlapIm.y;
-	imSizes.overlapIm.z -= imStarts.overlapIm.z;
+	localSizes.staticIm.x -= localStarts.staticIm.x;
+	localSizes.staticIm.y -= localStarts.staticIm.y;
+	localSizes.staticIm.z -= localStarts.staticIm.z;
+	localSizes.overlapIm.x -= localStarts.overlapIm.x;
+	localSizes.overlapIm.y -= localStarts.overlapIm.y;
+	localSizes.overlapIm.z -= localStarts.overlapIm.z;
+
+	imStarts.staticIm.x = localStarts.staticIm.x;
+	imStarts.staticIm.y = localStarts.staticIm.y;
+	imStarts.staticIm.z = localStarts.staticIm.z;
+	imStarts.overlapIm.x = localStarts.overlapIm.x;
+	imStarts.overlapIm.y = localStarts.overlapIm.y;
+	imStarts.overlapIm.z = localStarts.overlapIm.z;
+	imSizes.staticIm.x = localSizes.staticIm.x;
+	imSizes.staticIm.y = localSizes.staticIm.y;
+	imSizes.staticIm.z = localSizes.staticIm.z;
+	imSizes.overlapIm.x = localSizes.overlapIm.x;
+	imSizes.overlapIm.y = localSizes.overlapIm.y;
+	imSizes.overlapIm.z = localSizes.overlapIm.z;
 }
 
 void ridgidRegistration(const ImageContainer* staticImage, const ImageContainer* overlapImage, const Overlap& overlap,
