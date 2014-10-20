4f5da988ac1dd3c7db6d8c3fdb3b2f73f258cf02
Eric Wait <eric@waitphoto.com>
2014-10-20 15:32:30 -0400

    Fixed the edge reading code to apply the deltas properly

--- src/MATLAB/readDeltaData.m
+++ src/MATLAB/readDeltaData.m
@@ -15,7 +15,7 @@ for i=1:length(imageDatasets)
     if exist(filename,'file') 
         deltasPresent = 1;
         fid = fopen(filename,'rt');
-        data = fscanf(fid,'deltaX:%d\ndeltaY:%d\ndeltaZ:%d\nMaxCorr:%f\n');
+        data = fscanf(fid,'deltaX:%d\ndeltaY:%d\ndeltaZ:%d\nNCV:%f\n');
         deltaParent = [strtrim(fscanf(fid,'Parent:%255c\n')) '.'];
         fclose(fid);
         
@@ -31,24 +31,6 @@ for i=1:length(imageDatasets)
         imageDatasets(i).NCV = 0;
     end
     
-    imageDatasets(i).xMinPos = ...
-        imageDatasets(i).xDelta * imageDatasets(i).XPixelPhysicalSize + imageDatasets(i).YPosition*1e6;
-    
-    imageDatasets(i).xMaxPos = ...
-        imageDatasets(i).xMinPos + imageDatasets(i).XDimension * imageDatasets(i).XPixelPhysicalSize;
-    
-    imageDatasets(i).yMinPos = ...
-        imageDatasets(i).yDelta * imageDatasets(i).YPixelPhysicalSize + imageDatasets(i).XPosition*1e6;
-    
-    imageDatasets(i).yMaxPos = ...
-        imageDatasets(i).yMinPos + imageDatasets(i).YDimension * imageDatasets(i).YPixelPhysicalSize;
-    
-    imageDatasets(i).zMinPos = ...
-        imageDatasets(i).zDelta * imageDatasets(i).ZPixelPhysicalSize;
-    
-    imageDatasets(i).zMaxPos = ...
-        imageDatasets(i).zMinPos + imageDatasets(i).ZDimension * imageDatasets(i).ZPixelPhysicalSize;
-    
     imageDatasets(i).ParentDelta = find(cellfun(@(x)(~isempty(x)),strfind(names,deltaParent)));
     if isempty(imageDatasets(i).ParentDelta)
         imageDatasets(i).ParentDelta = i;
@@ -56,11 +38,50 @@ for i=1:length(imageDatasets)
 end
 
 if (deltasPresent==1)
-    imageDatasets(1).Child = [];
+    imageDatasets(1).Children = [];
+    root = [];
     for i=1:length(imageDatasets)
         if (imageDatasets(i).ParentDelta == i)
+            root = [root,i];
             continue;
         end
-        imageDatasets(imageDatasets(i).ParentDelta).Child = [imageDatasets(imageDatasets(i).ParentDelta).Child i];
+        imageDatasets(imageDatasets(i).ParentDelta).Children = [imageDatasets(imageDatasets(i).ParentDelta).Children i];
     end
+    
+    if (length(root)~=1), error('There are too many roots to min span tree!'); end
+    
+    imageDatasets = applyParentsDelta(root,0,0,0,imageDatasets);
+end
+end
+
+function imageDatasets = applyParentsDelta(root,deltaX,deltaY,deltaZ,imageDatasets)
+imageDatasets(root).xDelta = imageDatasets(root).xDelta + deltaX;
+imageDatasets(root).yDelta = imageDatasets(root).yDelta + deltaY;
+imageDatasets(root).zDelta = imageDatasets(root).zDelta + deltaZ;
+
+deltaX = imageDatasets(root).xDelta;
+deltaY = imageDatasets(root).yDelta;
+deltaZ = imageDatasets(root).zDelta;
+
+imageDatasets(root).xMinPos = ...
+    imageDatasets(root).xDelta * imageDatasets(root).XPixelPhysicalSize + imageDatasets(root).YPosition*1e6;
+
+imageDatasets(root).xMaxPos = ...
+    imageDatasets(root).xMinPos + imageDatasets(root).XDimension * imageDatasets(root).XPixelPhysicalSize;
+
+imageDatasets(root).yMinPos = ...
+    imageDatasets(root).yDelta * imageDatasets(root).YPixelPhysicalSize + imageDatasets(root).XPosition*1e6;
+
+imageDatasets(root).yMaxPos = ...
+    imageDatasets(root).yMinPos + imageDatasets(root).YDimension * imageDatasets(root).YPixelPhysicalSize;
+
+imageDatasets(root).zMinPos = ...
+    imageDatasets(root).zDelta * imageDatasets(root).ZPixelPhysicalSize;
+
+imageDatasets(root).zMaxPos = ...
+    imageDatasets(root).zMinPos + imageDatasets(root).ZDimension * imageDatasets(root).ZPixelPhysicalSize;
+
+for i=1:length(imageDatasets(root).Children)
+    imageDatasets = applyParentsDelta(imageDatasets(root).Children(i),deltaX,deltaY,deltaZ,imageDatasets);
+end
 end
