194bae0143a6b65a760ed6edeb1dc48d8306cc65
Eric Wait <eric@waitphoto.com>
2014-10-24 10:11:56 -0400

    Saves out the min span tree when not visualizing

--- src/MATLAB/combineImages.m
+++ src/MATLAB/combineImages.m
@@ -33,18 +33,26 @@ if (isempty(imageDatasets))
     error('No images for dataset %s\n',datasetName);
 end
 
+logDir = fullfile(imageDatasets(1).imageDir,'..','_GraphLog');
+if (~exist(logDir,'dir'))
+    mkdir(logDir);
+end
+
 [deltasPresent,imageDatasets] = readDeltaData(pathName,imageDatasets);
 
 if (0==deltasPresent)
     refine = questdlg('Would you like to refine registration or use microscope data?','Refine Deltas?','Refine','Microscope','Refine W/ Visualizer','Microscope');
+    if (refine==0), return, end
 else
     prefix = [datasetName '_Montage_wDelta'];
 end
 
 visualize = questdlg('Would you like to see the results?','Results Visualizer','Yes','No','Visualize Only','No');
+if (visulize==0), return, end
 numCudaDevices = CudaMex('DeviceCount');
 if (strcmp(visualize,'Visualize Only')==0 && numCudaDevices>0)
     reducIms = questdlg('Would you like to create reduced images?','Reduce Images','Yes','No','No');
+    if (reducIms==0), return, end
 else
     reducIms = 'No';
 end
@@ -52,6 +60,7 @@ end
 if (strcmp(refine,'Refine') || strcmp(refine,'Refine W/ Visualizer'))
     prefix = [datasetName '_Montage_wDelta'];
     answer = inputdlg('Channel to register:','Channel Chooser',1,{'3'});
+    if (answer==0), return, end
     imageDatasets = createDeltas(imageDatasets,str2double(answer),strcmp(refine,'Refine W/ Visualizer'));
     [~,imageDatasets] = readDeltaData(pathName,imageDatasets);
 elseif (0==deltasPresent)
@@ -94,7 +103,9 @@ clear im
 for chan=1:imageData.NumberOfChannels
     chanStart = tic;
     outImage = zeros(imageHeight,imageWidth,imageDepth,w.class);
-    outImageColor = zeros(imageHeight,imageWidth,imageDepth,w.class);
+    if (strcmp(visualize,'No')~=0)
+        outImageColor = zeros(imageHeight,imageWidth,imageDepth,w.class);
+    end
     fprintf('Chan:%d\n',chan);
     for datasetIdx=1:length(imageDatasets)
         if (imageDatasets(datasetIdx).NumberOfChannels>=chan)
@@ -124,10 +135,12 @@ for chan=1:imageData.NumberOfChannels
             
             clear nextIm
             
-            outImageColor(startYind:startYind+imageDatasets(datasetIdx).YDimension-1,...
-                startXind:startXind+imageDatasets(datasetIdx).XDimension-1,...
-                startZind:startZind+imageDatasets(datasetIdx).ZDimension-1) = ones(imageDatasets(datasetIdx).YDimension,...
-                imageDatasets(datasetIdx).XDimension,imageDatasets(datasetIdx).ZDimension)*datasetIdx;
+            if (strcmp(visualize,'No')~=0)
+                outImageColor(startYind:startYind+imageDatasets(datasetIdx).YDimension-1,...
+                    startXind:startXind+imageDatasets(datasetIdx).XDimension-1,...
+                    startZind:startZind+imageDatasets(datasetIdx).ZDimension-1) = ones(imageDatasets(datasetIdx).YDimension,...
+                    imageDatasets(datasetIdx).XDimension,imageDatasets(datasetIdx).ZDimension)*datasetIdx;
+            end
         end
     end
     
@@ -154,6 +167,12 @@ for chan=1:imageData.NumberOfChannels
         figure,imagesc(max(outImage,[],3)),colormap gray, axis image
         title(sprintf('Cannel:%d',chan),'Interpreter','none','Color','w');
         testingDeltas(outImage, outImageColor,imageDatasets,chan);
+    else
+        [fig,ax] = testingDeltas(outImage,[],imageDatasets,chan);
+        set(fig,'Units','normalized','Position',[0 0 1 1]);
+        frm = getframe(ax);
+        imwrite(frm.cdata,fullfile(logDir,sprintf('%s_c%02d_minSpanTree.tif',datasetName,chan)),'tif','Compression','lzw');
+        close(fig);
     end
     
     if (strcmp(reducIms,'Yes'))
@@ -188,7 +207,9 @@ for chan=1:imageData.NumberOfChannels
     end
     
     clear outImage;
-    clear outImageColor;
+    if (strcmp(visualize,'No')~=0)
+        clear outImageColor;
+    end
     fprintf('Chan:%d done in %f sec\n',chan,toc(chanStart));
 end
 fprintf('Completed in %f sec\n',toc(totalTime));
--- src/MATLAB/testingDeltas.m
+++ src/MATLAB/testingDeltas.m
@@ -1,14 +1,5 @@
-function testingDeltas(outImage, outImageColor,imageDatasets,chan)
-
-%% setup colormap for the colored image
-[img, colorIdx] = max(outImage(:,:,:),[],3);
-[r,c] = ndgrid(1:size(outImage,1),1:size(outImage,2));
-idx = sub2ind(size(outImage),r,c,colorIdx);
-pixelColors = outImageColor(idx);
-cmap = hsv(12);
-cmap = cmap(randi(12, max(double(pixelColors(:)))+1),:);
-alpha = cat(3, reshape(cmap(pixelColors+1,1),size(pixelColors)), reshape(cmap(pixelColors+1,2),size(pixelColors)), reshape(cmap(pixelColors+1,3),size(pixelColors)));
-imgC = alpha .* repmat(mat2gray(img),[1 1 3]);
+function [fig,ax] = testingDeltas(outImage, outImageColor,imageDatasets,chan)
+[img, colorIdx] = max(outImage,[],3);
 
 fig = figure;
 imagesc(img);
@@ -17,10 +8,21 @@ colormap(ax,'gray');
 
 drawBoxesLines(fig,ax,imageDatasets,chan);
 
-fig = figure;
-image(imgC);
-ax = gca;
-drawBoxesLines(fig,ax,imageDatasets,chan);
+if (~isempty(outImageColor))
+    %% setup colormap for the colored image
+    [r,c] = ndgrid(1:size(outImage,1),1:size(outImage,2));
+    idx = sub2ind(size(outImage),r,c,colorIdx);
+    pixelColors = outImageColor(idx);
+    cmap = hsv(12);
+    cmap = cmap(randi(12, max(double(pixelColors(:)))+1),:);
+    alpha = cat(3, reshape(cmap(pixelColors+1,1),size(pixelColors)), reshape(cmap(pixelColors+1,2),size(pixelColors)), reshape(cmap(pixelColors+1,3),size(pixelColors)));
+    imgC = alpha .* repmat(mat2gray(img),[1 1 3]);
+    
+    fig = figure;
+    image(imgC);
+    ax = gca;
+    drawBoxesLines(fig,ax,imageDatasets,chan);
+end
 
 drawnow
 
