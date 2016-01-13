6706900a616dfdb653a6a165c21d494647a7e1b2
Eric Wait <eric@waitphoto.com>
2016-01-13 10:12:34 -0500

    Added an option to refuse to make the final image locally.

--- src/MATLAB/+Registration/Run.m
+++ src/MATLAB/+Registration/Run.m
@@ -52,10 +52,12 @@ end
 
 visualizor = strcmp(refine,'Refine W/ Visualizer');
 
-visualize = questdlg('Would you like to see the results?','Results Visualizer','Yes','No','Visualize Only','No');
+visualize = questdlg('Would you like to see the NCV results?','Results Visualizer','Yes','No','Visualize Only','No');
 if (isempty(visualize)), return, end
 
 visualizor = visualizor || strcmp(visualize,'Yes') || strcmp(visualize,'Visualize Only');
+
+combineHere = questdlg('Would you like to make the final montage on this computer?','Combine Here?','Yes','No','No');
 
 %% Calculate the overlaps if they don't exist of the user wants them recalculated
 if (strcmp(refine,'Refine') || strcmp(refine,'Refine W/ Visualizer'))
@@ -66,148 +68,151 @@ elseif (0==deltasPresent)
     prefix = [datasetName '_Montage'];
 end
 
-%% Start a log dirctory
-logDir = fullfile(imageDatasets(1).imageDir,'..','_GraphLog');
-if (~exist(logDir,'dir'))
-    mkdir(logDir);
-end
-
-%% create a dirctory for the new images
-if ~isdir(fullfile(pathName,prefix))
-    mkdir(pathName,prefix);
-end
-
-%% Get the final dimensions
-[ imageData, minPos_XY, maxPos_XY ] = Registration.Overlap.GetFinalSize(imageDatasets,datasetName);
-tmpImageData = imageData;
-
-%% Get the image class to make memory for the new image
-imClass = MicroscopeData.GetImageClass(imageDatasets(1));
-
-%% Combine each channel and write it out due to memory constraints
-for chan=1:imageData.NumberOfChannels
-    chanStart = tic;
-    
-    % prealocate memory
-    outImage = zeros(maxPos_XY(2),maxPos_XY(1),maxPos_XY(3),imClass);
-    if (strcmp(visualize,'No')==false)
-        outImageColor = zeros(maxPos_XY(2),maxPos_XY(1),maxPos_XY(3),imClass);
+if (strcmp(combineHere,'Yes'))
+    %% Start a log dirctory
+    logDir = fullfile(imageDatasets(1).imageDir,'..','_GraphLog');
+    if (~exist(logDir,'dir'))
+        mkdir(logDir);
     end
     
-    % Give the user an indication of were the process is
-    fprintf('Chan:%d...',chan);
-    cp = Utils.CmdlnProgress(length(imageDatasets),true);
+    %% create a dirctory for the new images
+    if ~isdir(fullfile(pathName,prefix))
+        mkdir(pathName,prefix);
+    end
     
-    % Place each subimage into the final channel image
-    for datasetIdx=1:length(imageDatasets)
+    %% Get the final dimensions
+    [ imageData, minPos_XY, maxPos_XY ] = Registration.Overlap.GetFinalSize(imageDatasets,datasetName);
+    tmpImageData = imageData;
+    
+    %% Get the image class to make memory for the new image
+    imClass = MicroscopeData.GetImageClass(imageDatasets(1));
+    
+    %% Combine each channel and write it out due to memory constraints
+    for chan=1:imageData.NumberOfChannels
+        chanStart = tic;
         
-        % Ensure that this subimage has the current channel
-        if (imageDatasets(datasetIdx).NumberOfChannels>=chan)
-            % Get the image data
-            [nextIm,~] = MicroscopeData.ReaderParZ(imageDatasets(datasetIdx),[],chan,[],[],false,true);
-            
-            % Calculate where this sub image starts in the full image
-            startPos_xy = imageDatasets(datasetIdx).MinPos-minPos_XY+1;
-            endPos_xy = startPos_xy + min(imageDatasets(datasetIdx).Dimensions,size(nextIm))-1;
-            
-            outRoi = outImage(startPos_xy(2):endPos_xy(2),startPos_xy(1):endPos_xy(1),startPos_xy(3):endPos_xy(3));
-            
-            % Find out which image should exist in the overlap
-            % TODO this could be blended better
-            difInd = outRoi>0;
-            nextSum = sum(sum(sum(nextIm(difInd))));
-            outSum = sum(sum(sum(outRoi(difInd))));
-            if outSum>nextSum
-                nextIm(difInd) = outRoi(difInd);
-            end
-            clear outRoi
-            
-            % Set the output image with the sub image
-            outImage(startPos_xy(2):endPos_xy(2),startPos_xy(1):endPos_xy(1),startPos_xy(3):endPos_xy(3)) = nextIm;
-            clear nextIm
-            
-            % Create the colored output image if requested by the user
-            if (strcmp(visualize,'No')==0)
-                outImageColor(startPos_xy(2):endPos_xy(2),startPos_xy(1):endPos_xy(1),startPos_xy(3):endPos_xy(3)) =...
-                    ones(imageDatasets(datasetIdx).Dimensions(2),imageDatasets(datasetIdx).Dimensions(1),imageDatasets(datasetIdx).Dimensions(3))*datasetIdx;
-            end
+        % prealocate memory
+        outImage = zeros(maxPos_XY(2),maxPos_XY(1),maxPos_XY(3),imClass);
+        if (strcmp(visualize,'No')==false)
+            outImageColor = zeros(maxPos_XY(2),maxPos_XY(1),maxPos_XY(3),imClass);
         end
         
-        % Update the user with our progress
-        cp.PrintProgress(datasetIdx);
-    end
-    
-    % Cleanup the progress indicator
-    cp.ClearProgress();
-    
-    % Show the user the output if requested
-    if (strcmp(visualize,'Yes') || strcmp(visualize,'Visualize Only'))
-        figure,imagesc(max(outImage,[],3)),colormap gray, axis image
-        title(sprintf('Cannel:%d',chan),'Interpreter','none','Color','w');
-        Registration.Results.TestingDeltas(outImage, outImageColor,imageDatasets,chan,prefix);
-    end
-    
-    % Write out the resuts to a file
-    if (~strcmp(visualize,'Visualize Only'))
-        [fig,ax] = Registration.Results.TestingDeltas(outImage,[],imageDatasets,chan,prefix);
-        set(fig,'Units','normalized','Position',[0 0 1 1]);
-        if (size(outImage,1)>size(outImage,2))
-            camroll(ax,-90);
+        % Give the user an indication of were the process is
+        fprintf('Chan:%d...',chan);
+        cp = Utils.CmdlnProgress(length(imageDatasets),true);
+        
+        % Place each subimage into the final channel image
+        for datasetIdx=1:length(imageDatasets)
+            
+            % Ensure that this subimage has the current channel
+            if (imageDatasets(datasetIdx).NumberOfChannels>=chan)
+                % Get the image data
+                [nextIm,~] = MicroscopeData.ReaderParZ(imageDatasets(datasetIdx),[],chan,[],[],false,true);
+                
+                % Calculate where this sub image starts in the full image
+                startPos_xy = imageDatasets(datasetIdx).MinPos-minPos_XY+1;
+                endPos_xy = startPos_xy + min(imageDatasets(datasetIdx).Dimensions,size(nextIm))-1;
+                
+                outRoi = outImage(startPos_xy(2):endPos_xy(2),startPos_xy(1):endPos_xy(1),startPos_xy(3):endPos_xy(3));
+                
+                % Find out which image should exist in the overlap
+                % TODO this could be blended better
+                difInd = outRoi>0;
+                nextSum = sum(sum(sum(nextIm(difInd))));
+                outSum = sum(sum(sum(outRoi(difInd))));
+                if outSum>nextSum
+                    nextIm(difInd) = outRoi(difInd);
+                end
+                clear outRoi
+                
+                % Set the output image with the sub image
+                outImage(startPos_xy(2):endPos_xy(2),startPos_xy(1):endPos_xy(1),startPos_xy(3):endPos_xy(3)) = nextIm;
+                clear nextIm
+                
+                % Create the colored output image if requested by the user
+                if (strcmp(visualize,'No')==0)
+                    outImageColor(startPos_xy(2):endPos_xy(2),startPos_xy(1):endPos_xy(1),startPos_xy(3):endPos_xy(3)) =...
+                        ones(imageDatasets(datasetIdx).Dimensions(2),imageDatasets(datasetIdx).Dimensions(1),imageDatasets(datasetIdx).Dimensions(3))*datasetIdx;
+                end
+            end
+            
+            % Update the user with our progress
+            cp.PrintProgress(datasetIdx);
         end
-        frm = getframe(ax);
-        imwrite(frm.cdata,fullfile(logDir,sprintf('%s_c%02d_minSpanTree.tif',datasetName,chan)),'tif','Compression','lzw');
-        close(fig);
+        
+        % Cleanup the progress indicator
+        cp.ClearProgress();
+        
+        % Show the user the output if requested
+        if (strcmp(visualize,'Yes') || strcmp(visualize,'Visualize Only'))
+            figure,imagesc(max(outImage,[],3)),colormap gray, axis image
+            title(sprintf('Cannel:%d',chan),'Interpreter','none','Color','w');
+            Registration.Results.TestingDeltas(outImage, outImageColor,imageDatasets,chan,prefix);
+        end
+        
+        % Write out the resuts to a file
+        if (~strcmp(visualize,'Visualize Only'))
+            [fig,ax] = Registration.Results.TestingDeltas(outImage,[],imageDatasets,chan,prefix);
+            set(fig,'Units','normalized','Position',[0 0 1 1]);
+            if (size(outImage,1)>size(outImage,2))
+                camroll(ax,-90);
+            end
+            frm = getframe(ax);
+            imwrite(frm.cdata,fullfile(logDir,sprintf('%s_c%02d_minSpanTree.tif',datasetName,chan)),'tif','Compression','lzw');
+            close(fig);
+        end
+        
+        % Make sure the image is horizontal
+        % Rotate it if it is not and there is enough memory
+        w = whos('outImage');
+        userview = memory;
+        if (size(outImage,1)>size(outImage,2) && userview.MemAvailableAllArrays>w.bytes)
+            outImage = permute(outImage(end:-1:1,:,:),[2,1,3]);
+            tmpImageData.Dimensions = Utils.SwapXY_RC(imageData.Dimensions);
+        end
+        
+        % Save out the result
+        if (strcmp(visualize,'Visualize Only')==0)
+            imwrite(max(outImage,[],3),fullfile(pathName, prefix, ['_' datasetName sprintf('_c%02d_t%04d.tif',chan,1)]),'tif','Compression','lzw');
+            MicroscopeData.Writer(outImage,fullfile(pathName, [prefix, '\']),tmpImageData,[],chan);
+        end
+        
+        % Clean up this channel
+        clear outImage;
+        if (strcmp(visualize,'No')~=0)
+            clear outImageColor;
+        end
+        tm = toc(chanStart);
+        fprintf('done in %s\n',Utils.PrintTime(tm))
     end
     
-    % Make sure the image is horizontal
-    % Rotate it if it is not and there is enough memory
-    w = whos('outImage');
-    userview = memory;
-    if (size(outImage,1)>size(outImage,2) && userview.MemAvailableAllArrays>w.bytes)
-        outImage = permute(outImage(end:-1:1,:,:),[2,1,3]);
-        tmpImageData.Dimensions = Utils.SwapXY_RC(imageData.Dimensions);
-    end
+    tmpImageData.imageDir = fullfile(pathName, [prefix, '\']);
     
-    % Save out the result
+    clear outImageColor
+    clear difInd
+    
+    %% Save out overview results
     if (strcmp(visualize,'Visualize Only')==0)
-        imwrite(max(outImage,[],3),fullfile(pathName, prefix, ['_' datasetName sprintf('_c%02d_t%04d.tif',chan,1)]),'tif','Compression','lzw');
-        MicroscopeData.Writer(outImage,fullfile(pathName, [prefix, '\']),tmpImageData,[],chan);
+        % Save a colored maximum intensity version
+        imageData.imageDir = fullfile(pathName, [prefix, '\']);
+        colorMip = ImUtils.ThreeD.ColorMIP(MicroscopeData.ReaderParZ(tmpImageData,[],[],[],[],[],true),MicroscopeData.Colors.GetChannelColors(imageData));
+        imwrite(colorMip,fullfile(pathName,prefix,sprintf('_%s_RGB.tif',tmpImageData.DatasetName)),'tif','Compression','lzw');
+        f = figure;
+        imagesc(colorMip);%,'Parent',ax);
+        ax = get(f,'CurrentAxes');
+        %     if (tmpImageData.XDimension~=imageData.XDimension)
+        %         camroll(ax,90);
+        %     end
+        Registration.Results.DrawBoxesLines(f,ax,imageDatasets,0,tmpImageData.DatasetName);
+        %     if (size(colorMip,1)>size(colorMip,2))
+        %         camroll(ax,-90);
+        %     end
+        frm = getframe(ax);
+        imwrite(frm.cdata,fullfile(pathName,prefix,sprintf('_%s_graph.tif',tmpImageData.DatasetName)),'tif','Compression','lzw');
+        close(f);
+        clear colorMip
     end
     
-    % Clean up this channel
-    clear outImage;
-    if (strcmp(visualize,'No')~=0)
-        clear outImageColor;
-    end
-    tm = toc(chanStart);
-    fprintf('done in %s\n',Utils.PrintTime(tm))
-end
-
-tmpImageData.imageDir = fullfile(pathName, [prefix, '\']);
-
-clear outImageColor
-clear difInd
-
-%% Save out overview results
-if (strcmp(visualize,'Visualize Only')==0)
-    % Save a colored maximum intensity version
-    imageData.imageDir = fullfile(pathName, [prefix, '\']);
-    colorMip = ImUtils.ThreeD.ColorMIP(MicroscopeData.ReaderParZ(tmpImageData,[],[],[],[],[],true),MicroscopeData.Colors.GetChannelColors(imageData));
-    imwrite(colorMip,fullfile(pathName,prefix,sprintf('_%s_RGB.tif',tmpImageData.DatasetName)),'tif','Compression','lzw');
-    f = figure;
-    imagesc(colorMip);%,'Parent',ax);
-    ax = get(f,'CurrentAxes');
-%     if (tmpImageData.XDimension~=imageData.XDimension)
-%         camroll(ax,90);
-%     end
-    Registration.Results.DrawBoxesLines(f,ax,imageDatasets,0,tmpImageData.DatasetName);
-%     if (size(colorMip,1)>size(colorMip,2))
-%         camroll(ax,-90);
-%     end
-    frm = getframe(ax);
-    imwrite(frm.cdata,fullfile(pathName,prefix,sprintf('_%s_graph.tif',tmpImageData.DatasetName)),'tif','Compression','lzw');
-    close(f);
-    clear colorMip
 end
 
 %% Done!
