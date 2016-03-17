bd6299ea6c1149d7624a5aebb2aa00770268b105
Eric Wait <eric@waitphoto.com>
2016-03-17 15:19:33 -0400

    Updated SVZ mask making

--- src/MATLAB/+Registration/+Results/MakeSVZMask.m
+++ src/MATLAB/+Registration/+Results/MakeSVZMask.m
@@ -1,75 +1,56 @@
-function MakeSVZMask(montages,redoAll,prompt)
-
-if (~exist('redoAll','var') || isempty(redoAll))
-    redoAll = false;
+function MakeSVZMask()
+imD = MicroscopeData.ReadMetadata();
+%% get the iamge data
+mipfilePath = fullfile(imD.imageDir,[imD.DatasetName,'_MIP.tif']);
+if (~exist(mipfilePath,'file'))
+    mipfilePath = fullfile(imD.imageDir,['_',imD.DatasetName,'_MIP.tif']);
+    if (~exist(mipfilePath,'file'))
+        error('Cannot find file %s',mipfilePath);
+    end
 end
-if (~exist('prompt','var') || isempty(prompt))
-    prompt = false;
-end
+    
+colorMip = imread(mipfilePath);
+colors = MicroscopeData.Colors.GetChannelColors(imD);
+    
+bw = roipoly(colorMip);
 
-for i=1:length(montages)
-    %% get the iamge data
-    [im,imageData] = MicroscopeData.Reader(montages(i).filePath);
-    colors = MicroscopeData.GetChannelColors(imageData);
-    colorMip = ImUtils.ThreeD.ColorMIP(im,colors);
+imwrite(im2uint8(bw),fullfile(imD.imageDir,['_',imD.DatasetName,'_Mask.tif']),'compression','lzw');
     
-    maskPath = fullfile(montages(i).filePath,['_',imageData.DatasetName, '_mask.tif']);
+%% get the boundry pixels dialated a bit
+bound = bwperim(bw);
+se = strel('disk',5,4);
+bound = imdilate(bound,se);
+boundInd = bound>0;
     
-    %% read the mask image or create it
-    if (exist(maskPath,'file') && ~redoAll)
-        if (prompt)
-            rspnce = questdlg('Would you like to redraw?','Redo ROI','Yes','No','No');
-            if (strcmpi('yes',rspnce))
-                bw = roipoly(colorMip);
-                if (isempty(bw))
-                    continue
-                end
-            else
-                bw = imread(maskPath);
-            end
-        else
-            bw = imread(maskPath);
-        end
-    else
-        bw = roipoly(colorMip);
+%% get the number of image combinations we want
+masks = zeros(2^imD.NumberOfChannels,imD.NumberOfChannels);
+for i=1:2^imD.NumberOfChannels
+    masks(i,:) = bitget(i,imD.NumberOfChannels:-1:1);
+end
+masks = masks>0;
+
+prgs = Utils.CmdlnProgress(size(masks,1),false,'Making Color Combinations');
+chans = 1:imD.NumberOfChannels;
+for j=1:size(masks,1)
+    curChans = chans(masks(j,:));
+    if (isempty(curChans))
+        continue
     end
     
-    %% get the boundry pixels dialated a bit
-    bound = bwperim(bw);
-    se = strel('disk',5,4);
-    bound = imdilate(bound,se);
-    boundInd = bound>0;
+    imageName = sprintf('_%s_chan%s.tif',imD.DatasetName,num2str(curChans,'%d'));
+    imagePath = fullfile(imD.imageDir,imageName);
     
-    %% get the number of image combinations we want
-    masks = Utils.MakeBinaryMasks(imageData.NumberOfChannels);
-    masks(1:end-1,1) = false(numel(1:size(masks,1)-1),1);
-    masks = unique(masks,'rows');
+    im = MicroscopeData.ReaderParZ(imD,[],curChans);
+    colorMip = ImUtils.ThreeD.ColorMIP(im,colors(curChans,:));
+    imwrite(colorMip,imagePath,'tif','Compression','lzw');
     
-    prgs = Utils.CmdlnProgress(size(masks,1),false);
-    chans = 1:imageData.NumberOfChannels;
-    for j=1:size(masks,1)
-        curChans = chans(masks(j,:));
-        if (isempty(curChans))
-            continue
-        end
-        
-        imageName = sprintf('_%s_chan%s.tif',imageData.DatasetName,num2str(curChans,'%d'));
-        imagePath = fullfile(montages(i).filePath,imageName);
-        imageNameMask = sprintf('_%s_masked_chan%s.tif',imageData.DatasetName,num2str(curChans,'%d'));
-        imagePathMask = fullfile(montages(i).filePath,imageNameMask);
-        
-        if (~exist(imagePath,'file') || ~exist(imagePathMask,'file') || redoAll)
-            colorMip = ImUtils.ThreeD.ColorMIP(im(:,:,:,curChans),colors(curChans,:));            
-            imwrite(colorMip,imagePath,'tif','Compression','lzw');
-            
-            colorMip = ImUtils.ROI.GetMaskedIm(colorMip,bw,boundInd);
-            imwrite(colorMip,imagePath,'tif','Compression','lzw');
-            
-            fprintf('Wrote: %s\n',imageName);
-        end
-        prgs.PrintProgress(j);
-    end
+    colorMip = ImUtils.ROI.GetMaskedIm(colorMip,bw,boundInd);
+    imwrite(colorMip,imagePath,'tif','Compression','lzw');
     
-    prgs.ClearProgress();
+    fprintf('Wrote: %s\n',imageName);
+    prgs.PrintProgress(j);
 end
+
+prgs.ClearProgress(true);
+
 end
