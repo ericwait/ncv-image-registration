93ac72f583ae0d474f54b91c9c2e55bd0f74a2d2
Eric Wait <eric@waitphoto.com>
2017-03-17 20:54:57 -0400

    Changed how the subsection's metadata is collected

--- src/MATLAB/+Registration/GetMontageSubMeta.m
+++ src/MATLAB/+Registration/GetMontageSubMeta.m
@@ -6,53 +6,67 @@ imageDatasets = [];
 datasetName = '';
 
 if (~exist('listPath','var') || isempty(listPath))
-    [fileName,pathName,~] = uigetfile('.txt');
+    [fileName,pathName,~] = uigetfile({'*.txt';'*.*'});
     if fileName==0
-            warning('No data read');
-            return
+        warning('No data read');
+        return
     end
 else
     [pathName,fileName,ext] = fileparts(listPath);
     fileName = [fileName ext];
 end
 
-fHand = fopen(fullfile(pathName,fileName),'rt');
-dirNames = textscan(fHand,'%s','delimiter','\n');
-fclose(fHand);
+[~,~,ext] = fileparts(fileName);
 
-datasetName = [];
-str1 = dirNames{1}{1};
-for i=length(str1):-1:1
-    bmatch = strncmpi(str1(1:i),dirNames{1},i);
-    if nnz(bmatch)==length(dirNames{1})
-        datasetName = str1(1:i);
-        break
-    end
-end
-
-for i=1:length(dirNames{1})
-    [~,~,ext] = fileparts(dirNames{1}{i});
-    if (strcmpi(ext,'.txt'))
-        metaFilePath = fullfile(pathName,dirNames{1}{i});
-    elseif (strcmpi(ext,'.json'))
-        metaFilePath = fullfile(pathName,dirNames{1}{i},[dirNames{1}{i},'.json']);
-    else
-        metaFilePath = fullfile(pathName,dirNames{1}{i},[dirNames{1}{i},'.txt']);
-        if (~exist(metaFilePath,'file'))
+if (strcmpi(ext,'.txt'))
+    fHand = fopen(fullfile(pathName,fileName),'rt');
+    dirNames = textscan(fHand,'%s','delimiter','\n');
+    fclose(fHand);
+        
+    for i=1:length(dirNames{1})
+        [~,~,ext] = fileparts(dirNames{1}{i});
+        if (strcmpi(ext,'.txt'))
+            metaFilePath = fullfile(pathName,dirNames{1}{i});
+        elseif (strcmpi(ext,'.json'))
             metaFilePath = fullfile(pathName,dirNames{1}{i},[dirNames{1}{i},'.json']);
+        else
+            metaFilePath = fullfile(pathName,dirNames{1}{i},[dirNames{1}{i},'.txt']);
             if (~exist(metaFilePath,'file'))
-                metaFilePath = [];
+                metaFilePath = fullfile(pathName,dirNames{1}{i},[dirNames{1}{i},'.json']);
+                if (~exist(metaFilePath,'file'))
+                    metaFilePath = [];
+                end
+            end
+        end
+        
+        if (~isempty(metaFilePath))
+            if isempty(imageDatasets)
+                [imageDatasets,~] = MicroscopeData.ReadMetadata(metaFilePath);
+            else
+                [imageDatasets(end+1),~] = MicroscopeData.ReadMetadata(metaFilePath);
             end
         end
     end
-    
-    if (~isempty(metaFilePath))
-        if isempty(imageDatasets)
-            [imageDatasets,~] = MicroscopeData.ReadMetadata(metaFilePath);
-        else
-            [imageDatasets(end+1),~] = MicroscopeData.ReadMetadata(metaFilePath);
-        end
+else
+    [~,imageDatasets] = MicroscopeData.Original.ConvertData(pathName,fileName,[],true,true,false,true);
+    imageDatasets = cellfun(@(x)(x),imageDatasets);
+    for i=1:length(imageDatasets)
+        imageDatasets(i).imageDir = fullfile(pathName,imageDatasets(i).DatasetName);
     end
 end
+
+datasetName = '';
+dNames = arrayfun(@(x)(x.DatasetName),imageDatasets,'UniformOutput',false);
+lengths = cellfun(@(x)(length(x)),dNames);
+for i=1:min(lengths)
+    letter = dNames{1}(i);
+    match = cellfun(@(x)(strcmp(x(i),letter)),dNames);
+    if (all(match))
+        datasetName = [datasetName,letter];
+    else
+        break
+    end
+end
+datasetName = MicroscopeData.Helper.SanitizeString(datasetName);
 end
 
