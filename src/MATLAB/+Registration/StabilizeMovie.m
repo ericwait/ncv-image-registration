ddb3921040f5df97280bf5318aac373308551db4
Eric Wait <eric@waitphoto.com>
2018-07-14 13:00:57 -0400

    Return the results from StabilizeMovie for subsequent runs.

--- src/MATLAB/+Registration/StabilizeMovie.m
+++ src/MATLAB/+Registration/StabilizeMovie.m
@@ -1,10 +1,12 @@
-function [imOut, imageDataOut] = StabilizeMovie(imIn, imageDataIn,unitFactor,minOverlap,maxSearchSize,logFile,showDecisionSurf,visualize, imMask1, imMask2)
-%RUN Takes a movie and attempts to stabilize the objects between frames in
-%   a using a global rigid registration
-%[imOut, imageDataOut] = Stabilize.Run(imIn, imageDataIn,unitFactor,minOverlap,maxSearchSize,logFile,showDecisionSurf,visualize, imMask1, imMask2)
+function [imOut, imageDataOut,cumulativeDeltas_rc] = StabilizeMovie(imIn,imageDataIn,cumulativeDeltas_rc,unitFactor,minOverlap,maxSearchSize,logFile,showDecisionSurf,visualize, imMask1, imMask2)
+%RUN Takes a movie and attempts to stabilize the objects between frames by
+% translation of sebsequent frames
+%[imOut, imageDataOut,cumulativeDeltas_rc] = StabilizeMovie(imIn,imageDataIn,cumulativeDeltas_rc,unitFactor,minOverlap,maxSearchSize,logFile,showDecisionSurf,visualize, imMask1, imMask2)
 %imIn - This should be a 5D image as (x,y,z,chan,time);
 %imageDataIn - This should be a well formed metadata structure. See MicroscopeData.GetEmptyMetadata
 %The following are optional paramaters:
+%       cumulativeDeltas_rc - these are the deltas that will be applied to
+%          the sequence without running interframe registration
 %       unitFactor - this is a multiplyer to get the position data into the
 %           same units as the PixelPhysicalSize
 %       minOverlap - this is the 
@@ -22,6 +24,9 @@ function [imOut, imageDataOut] = StabilizeMovie(imIn, imageDataIn,unitFactor,min
 
 
 %% check inputs
+    if (~exist('cumulativeDeltas_rc','var'))
+        cumulativeDeltas_rc = [];
+    end
     if (~exist('unitFactor','var'))
         unitFactor = [];
     end
@@ -46,59 +51,63 @@ function [imOut, imageDataOut] = StabilizeMovie(imIn, imageDataIn,unitFactor,min
     if (~exist('imMask2','var'))
         imMask2 = [];
     end
-
-    %% calculate the size of the parallel pool needed
-    numVoxels = prod(imageDataIn.Dimensions);
-    w = whos('imIn');
-    memNeededBytes = numVoxels*8*8 + 2*w.bytes;
-    m = memory;
-    pc = parcluster('local');
     
-    numWorkers = floor((m.MemAvailableAllArrays*0.80)/memNeededBytes);
-    numWorkers = min(pc.NumWorkers,numWorkers);
-    
-    p = gcp('nocreate');
-    oldWorkers = 0;
-    if (isvalid(p))
-        oldWorkers = p.NumWorkers;
-    end
-    if (oldWorkers==0)
-        parpool(numWorkers);
-    elseif (oldWorkers>numWorkers)
-        delete(p);
-        parpool(numWorkers);
-    end    
+    is = tic;
     
 %% run the reg
-    is = tic;
-    imDt = imageDataIn;
-    imDt.NumberOfFrames = 1;
-    imDt1 = imDt;
-    posT = imageDataIn.Position;
+    if (isempty(cumulativeDeltas_rc))
+    % calculate the size of the parallel pool needed
     
-    frameDeltas_xyz = zeros(imageDataIn.NumberOfFrames,5);
-    parfor t=1:imageDataIn.NumberOfFrames-1
-        curFrame = imIn(:,:,:,:,t);
-        curD = imDt;
-        curD.Position = [0,0,0];
-        %curD.Position = squeeze(posT(1,1,t,:))';
-        nextFrame = imIn(:,:,:,:,t+1);
-        nextD = imDt;
-        nextD.Position = [0,0,0];
-        %nextD.Position = squeeze(posT(1,1,t+1,:))';
-        
-        [deltaX,deltaY,deltaZ,maxNCV,overlapSize] = Registration.FFT.RegisterTwoImages(curFrame,curD,nextFrame,nextD,unitFactor,minOverlap,maxSearchSize,logFile,showDecisionSurf,visualize, imMask1, imMask2);
-        
-        if (isinf(maxNCV))
-            [deltaX,deltaY,deltaZ,maxNCV,overlapSize] = Registration.FFT.RegisterTwoImages(curFrame,curD,nextFrame,nextD,unitFactor,minOverlap,maxSearchSize,logFile,true,true, imMask1, imMask2);
-            title(num2str(t));
+        numVoxels = prod(imageDataIn.Dimensions);
+        w = whos('imIn');
+        memNeededBytes = numVoxels*8*8 + 2*w.bytes;
+        m = memory;
+        pc = parcluster('local');
+
+        numWorkers = floor((m.MemAvailableAllArrays*0.80)/memNeededBytes);
+        numWorkers = min(pc.NumWorkers,numWorkers);
+
+        p = gcp('nocreate');
+        oldWorkers = 0;
+        if (isvalid(p))
+            oldWorkers = p.NumWorkers;
         end
-        
-        frameDeltas_xyz(t+1,:) = [deltaX,deltaY,deltaZ,maxNCV,overlapSize];
-    end
+        if (oldWorkers==0)
+            parpool(numWorkers);
+        elseif (oldWorkers>numWorkers)
+            delete(p);
+            parpool(numWorkers);
+        end    
     
-    frameDeltas_rc = Utils.SwapXY_RC(frameDeltas_xyz);
-    cumulativeDeltas_rc = cumsum(frameDeltas_rc(:,1:3),1);
+        imDt = imageDataIn;
+        imDt.NumberOfFrames = 1;
+
+        frameDeltas_xyz = zeros(imageDataIn.NumberOfFrames,5);
+        parfor t=1:imageDataIn.NumberOfFrames-1
+            curFrame = imIn(:,:,:,:,t);
+            curD = imDt;
+            curD.Position = [0,0,0];
+            %curD.Position = squeeze(posT(1,1,t,:))';
+            nextFrame = imIn(:,:,:,:,t+1);
+            nextD = imDt;
+            nextD.Position = [0,0,0];
+            %nextD.Position = squeeze(posT(1,1,t+1,:))';
+
+            [deltaX,deltaY,deltaZ,maxNCV,overlapSize] = Registration.FFT.RegisterTwoImages(curFrame,curD,nextFrame,nextD,unitFactor,minOverlap,maxSearchSize,logFile,false, imMask1, imMask2);
+
+            if (isinf(maxNCV))
+                [deltaX,deltaY,deltaZ,maxNCV,overlapSize] = Registration.FFT.RegisterTwoImages(curFrame,curD,nextFrame,nextD,unitFactor,minOverlap,maxSearchSize,logFile,true, imMask1, imMask2);
+                title(num2str(t));
+            end
+
+            frameDeltas_xyz(t+1,:) = [deltaX,deltaY,deltaZ,maxNCV,overlapSize];
+        end
+
+        frameDeltas_rc = Utils.SwapXY_RC(frameDeltas_xyz);
+        cumulativeDeltas_rc = cumsum(frameDeltas_rc(:,1:3),1);
+    end
+
+%% apply deltas
     maxDelta_rc = max(cumulativeDeltas_rc,[],1);
     minDelta_rc = min(cumulativeDeltas_rc,[],1);
     
@@ -113,15 +122,17 @@ function [imOut, imageDataOut] = StabilizeMovie(imIn, imageDataIn,unitFactor,min
         imOut(newPosStart(1):newPosEnd(1),newPosStart(2):newPosEnd(2),newPosStart(3):newPosEnd(3),:,t) = imIn(:,:,:,:,t);
     end
     
-    if (oldWorkers~=0 && numWorkers~=oldWorkers)
+    if (exist('oldWorkers','var') && oldWorkers~=0 && numWorkers~=oldWorkers)
         p = gcp('nocreate');
         delete(p);
         parpool(oldWorkers);
     end
     
     imageDataOut = imageDataIn;
+    imageDataOut.DatasetName = [imageDataOut.DatasetName,'_registered'];
     sz = size(imOut);
     imageDataOut.Dimensions = sz([2,1,3]);
-    fprintf('Image Stabilization Took: %s\n',Utils.PrintTime(toc(is)))
+    sec = toc(is);
+    fprintf('Image Stabilization Took: %s, avg: %s\n',Utils.PrintTime(sec),Utils.PrintTime(sec/size(imIn,5)));
 end
 
