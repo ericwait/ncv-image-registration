9cd6861230cfc86cfe342744609f49849c89505c
brossetti <rossettib@janelia.hhmi.org>
2019-11-18 12:54:25 -0500

    feature: added two other modes, 'fit' and 'crop', that define alternative reference coordinate systems

--- src/MATLAB/+Registration/ShiftImages.m
+++ src/MATLAB/+Registration/ShiftImages.m
@@ -1,27 +1,67 @@
-function newIm = ShiftImages(im1,im2,deltaX,deltaY,deltaZ)
-% Shift image 2 realitive to image 1. Positive delta moves the origin 
-%   of image 2 further into image 1.
-% Image 1 will be in the first channel and Image 2 will be in the second.
+function newIm = ShiftImages(im1,im2,deltaX,deltaY,deltaZ,mode)
+% Shift im2 realitive to im1. Positive deltas move the origin of im2 
+% in the positive direction w.r.t. im1. A two channel registered image is 
+% returned in which the first channel is im1 and the second channel is im2.
+% There are three shift modes: pad (default), fit, and crop. Pad returns an image
+% that is at least as large as the input images, and contains zero padding
+% where the images do not overlap. Fit returns an image equal to the size
+% of im1, and it maintains the reference coordinate system of im1. Crop
+% returns an image at most as large as the input images, and contains only
+% the intersection area.
 
-    deltas_rc = [deltaY,deltaX,deltaZ];
+    if (~exist('mode','var') || isempty(mode))
+        mode = 'pad';
+    end
     
     im1Sz = ImUtils.Size(im1);
     im2Sz = ImUtils.Size(im2);
+    
+    deltas = [deltaY,deltaX,deltaZ];
+    
+    posDeltas = max(deltas,[0,0,0]);
+    negDeltas = min(deltas,[0,0,0]);
+    
+    refStart = [1,1,1] + posDeltas;
+    refEnd = im1Sz(1:3) + negDeltas;
+    trgtStart = [1,1,1] - negDeltas;
+    trgtEnd = im2Sz(1:3) - posDeltas;
+       
+    if strcmp(mode,'pad')
+        padSz = im1Sz(1:3) + abs(deltas);
+        newIm = zeros([padSz,2,size(im1,5)],class(im1));
 
-    im1Start_rc = max(-deltas_rc+1,[1,1,1]);
-    im2Start_rc = max(deltas_rc+1,[1,1,1]);
-    
-    im1End_rc = im1Start_rc +im1Sz(1:3) -1;
-    im2End_rc = im2Start_rc +im2Sz(1:3) -1;
-    
-    newSize = max(im1End_rc,im2End_rc);
-    newIm = zeros([newSize,2,size(im1,5)],class(im1));
-    
-    newIm(im1Start_rc(1):im1End_rc(1),...
-          im1Start_rc(2):im1End_rc(2),...
-          im1Start_rc(3):im1End_rc(3),1,:) = im1;
-      
-    newIm(im2Start_rc(1):im2End_rc(1),...
-          im2Start_rc(2):im2End_rc(2),...
-          im2Start_rc(3):im2End_rc(3),2,:) = im2;        
+        refEnd = padSz + negDeltas;
+        trgtEnd = padSz - posDeltas;
+        
+        newIm(trgtStart(1):trgtEnd(1),...
+              trgtStart(2):trgtEnd(2),...
+              trgtStart(3):trgtEnd(3),1,:) = im1;
+
+        newIm(refStart(1):refEnd(1),...
+              refStart(2):refEnd(2),...
+              refStart(3):refEnd(3),2,:) = im2;
+    elseif strcmp(mode,'fit')
+        newIm = zeros([im1Sz(1),im1Sz(2),im1Sz(3),2,im1Sz(5)],class(im1));
+
+        newIm(:,:,:,1,:) = im1;
+
+        newIm(refStart(1):refEnd(1),...
+              refStart(2):refEnd(2),...
+              refStart(3):refEnd(3),2,:) = im2(trgtStart(1):trgtEnd(1),...
+                                               trgtStart(2):trgtEnd(2),...
+                                               trgtStart(3):trgtEnd(3));
+    elseif strcmp(mode,'crop')
+        cropSz = im1Sz(1:3) - abs(deltas);
+        newIm = zeros([cropSz,2,im1Sz(5)],class(im1));
+
+        newIm(:,:,:,1,:) = im1(refStart(1):refEnd(1),...
+                               refStart(2):refEnd(2),...
+                               refStart(3):refEnd(3));
+
+        newIm(:,:,:,2,:) = im2(trgtStart(1):trgtEnd(1),...
+                               trgtStart(2):trgtEnd(2),...
+                               trgtStart(3):trgtEnd(3));
+    else
+        error('Not a valid shift mode');
+    end
 end
